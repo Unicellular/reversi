5de6ef210a32a7e4aa49186ade57d30f67408d68
Steven J. Li <StevenJLi@gmail.com>
2013-04-25 00:02:52 -0700

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+We built a command-line version of the classic game of Reversi (aka Othello) using Ruby.
---
+++ game.rb
@@ -0,0 +1,62 @@
+
+require_relative './board'
+require_relative './piece'
+require_relative './player'
+require_relative './position'
+
+class Game
+  attr_reader :board, :current_color
+
+  def initialize(player1, player2, board = Board.new)
+    @players = {
+      :white => player1,
+      :black => player2
+    }
+    @board = board
+    @current_color = :white
+  end
+
+  def play
+    until board.result
+      begin
+        move(current_player.get_move(board))
+        change_player
+      rescue InvalidMoveError
+        retry
+      end
+    end
+    
+    print_game_result
+  end
+
+  private
+  def current_player
+    @players[current_color]
+  end
+
+  def change_player
+    @current_color = (@current_color == :black) ? :white : :black
+  end
+
+  def move(move_pos)
+    Piece.place(board, move_pos, current_color)
+  end
+  
+  def print_game_result
+
+    board.print
+
+    case board.result
+    when :black
+      puts "Black has won!"
+    when :white
+      puts "White has won!"
+    when :draw
+      puts "No one wins!"
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Game.new(Player.new, Player.new).play
+end
