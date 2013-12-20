a64928b0fc08df8d433bd94cffc2b5bdbe845796
Rex <rexkimta@gmail.com>
2013-12-20 11:41:37 +0800

    show the decision greedy AI make

--- greeder.rb
+++ greeder.rb
@@ -1,11 +1,12 @@
 module Reversi
   class Greeder
     def move( board, color )
-      puts "#{color}'s move, thinking..."
       valid_moves = board.valid_moves( color )
-      valid_moves.max_by do |move| 
+      result = valid_moves.max_by do |move| 
 	board.pieces_to_flip( move, color ).length
       end
+      puts "#{color}'s turn, thinking...#{result.row},#{result.col}" 
+      result
     end
   end
 end
